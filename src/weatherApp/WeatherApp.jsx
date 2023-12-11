f7469e1f1150e9de9d9b87240fb726b5503d2702
codifyLE <l13121915@gmail.com>
2023-12-11 20:42:33 +0300

    weather

--- src/weatherApp/WeatherApp.jsx
+++ src/weatherApp/WeatherApp.jsx
@@ -31,12 +31,12 @@ const WeatherApp = () => {
     }
     setLocation("");
   };
-console.log(locInfo)
+  console.log(locInfo);
   return (
     <div className="weatherApp">
       <div className="container">
         <div className="items">
-        <div className="search">
+          <div className="search">
             <input
               type="text"
               value={location}
@@ -69,22 +69,22 @@ console.log(locInfo)
               </div>
             )}
           </div>
-          {locInfo.name != undefined &&   
-          <div className="bottom">
-          <div className="feels">
+          {locInfo.name != undefined && (
+            <div className="bottom">
+              <div className="feels">
                 <h2>Feels like</h2>
-               {locInfo.feels &&  <p>{locInfo.feels}°C</p>}
-               </div>
-               <div className="wind">
+                {locInfo.feels && <p>{locInfo.feels}°C</p>}
+              </div>
+              <div className="wind">
                 <h2>Wind Speed</h2>
-               {locInfo.wind &&  <p>{locInfo.wind} MPH</p>}
-               </div>
-               <div className="humidity">
+                {locInfo.wind && <p>{locInfo.wind} MPH</p>}
+              </div>
+              <div className="humidity">
                 <h2>Humidity</h2>
-               {locInfo.humidity &&  <p>{locInfo.humidity}%</p>}
-               </div>
-          </div>}
-    
+                {locInfo.humidity && <p>{locInfo.humidity}%</p>}
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
